4be532bdbfbd3da73f1c5bae83335d9466f61ecf
Sunny Patel <sunnypatel2048@gmail.com>
2023-10-17 21:23:48 +0530

    Changes

--- handlers/task_handler.go
+++ handlers/task_handler.go
@@ -9,16 +9,16 @@ import (
 	"github.com/sunnypatel2048/basic-tasks-api/models"
 )
 
-var tasks []models.Task
+var Tasks []models.Task
 
 func GetTasks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(tasks)
+	json.NewEncoder(w).Encode(Tasks)
 }
 
 func GetTask(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	for _, item := range tasks {
+	for _, item := range Tasks {
 		if item.ID == params["id"] {
 			w.Header().Set("Content-Type", "application/json")
 			json.NewEncoder(w).Encode(item)
@@ -26,41 +26,48 @@ func GetTask(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	w.WriteHeader(http.StatusNotFound)
+	errorResponse := map[string]string{"error": "Task not found"}
+	json.NewEncoder(w).Encode(errorResponse)
 }
 
 func CreateTask(w http.ResponseWriter, r *http.Request) {
 	var task models.Task
 	_ = json.NewDecoder(r.Body).Decode(&task)
-	task.ID = fmt.Sprintf("%d", len(tasks)+1)
-	tasks = append(tasks, task)
+	task.ID = fmt.Sprintf("%d", len(Tasks)+1)
+	Tasks = append(Tasks, task)
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(task)
 }
 
 func UpdateTask(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	for i, item := range tasks {
+	var updatedTask models.Task
+	_ = json.NewDecoder(r.Body).Decode(&updatedTask)
+
+	for i, item := range Tasks {
 		if item.ID == params["id"] {
-			var task models.Task
-			_ = json.NewDecoder(r.Body).Decode(&task)
-			task.ID = params["id"]
-			tasks[i] = task
+			Tasks[i] = updatedTask
 			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(task)
+			json.NewEncoder(w).Encode(updatedTask)
 			return
 		}
 	}
 	w.WriteHeader(http.StatusNotFound)
+	errorResponse := map[string]string{"error": "Task not found"}
+	json.NewEncoder(w).Encode(errorResponse)
 }
 
 func DeleteTask(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	for i, item := range tasks {
+	for i, item := range Tasks {
 		if item.ID == params["id"] {
-			tasks = append(tasks[:i], tasks[i+1:]...)
+			Tasks = append(Tasks[:i], Tasks[i+1:]...)
 			w.WriteHeader(http.StatusNoContent)
 			return
 		}
 	}
 	w.WriteHeader(http.StatusNotFound)
+	errorResponse := map[string]string{"error": "Task not found"}
+	json.NewEncoder(w).Encode(errorResponse)
 }
